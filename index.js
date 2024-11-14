880aac9715e13308f1b7eb1e38005ada9e7de947
Donald Motswiri <domotswiri@gmail.com>
2024-11-14 14:01:02 +0200

    lit and broken state

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('initial')
+function lightBulb() {
+    let isLit = false
+    let isBroken = false
+
+    return {
+        state() {
+            return { isLit, isBroken }
+        },
+        toggle() {
+            isLit = !isLit
+        },
+        break() {
+            isBroken = true
+        }
+    }
+}
+
+const bulb = lightBulb()
+const log = () => {
+    console.log(bulb.state())
+}
+
+bulb.toggle()
+bulb.break()
+log()
