60db2ccf8029ee0cebfe41fef2d808784fbc47ee
Donald Motswiri <domotswiri@gmail.com>
2024-11-15 16:36:00 +0200

    add-actions

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-const { Machine, interpret } = require('xstate')
+const { Machine, interpret } = require('xstate');
 
 const lit = {
     on: {
         BREAK: 'broken',
-        TOGGLE: 'unlit'
-    }
-}
+        TOGGLE: 'unlit',
+    },
+};
 
 const unlit = {
     on: {
-        BREAK: 'broken',
-        TOGGLE: 'lit'
+        BREAK: {
+            target: 'broken',
+            actions: ['logBroken'],
+        },
+        TOGGLE: 'lit',
+    },
+};
+
+const broken = { type: 'final' };
+
+const states = { lit, unlit, broken };
+
+const actions = {
+    logBroken: (context, event) => {
+        console.log(context, event);
+    },
+};
+
+const lightBulbMachine = Machine(
+    {
+        /** @xstate-layout N4IgpgJg5mDOIC5QBsCWUAWAXAQgV2QCMA6NLAYhwCUBRAQQGkBtABgF1FQAHAe1lSyoeAO04gAHogDMADhnEWAFgBsATgCM6lkpUB2AKwAaEAE9EAJnP6FU9VakGW51ftcBfN8bSZcBEmXIAFQB5AHFQgBkaVg4kEF5+QRExSQR1ZSliVRlFWXU5RRVbc2MzNJkWYk1FfVVzJRlzXRZ1XQ8vdGx8ImI8YQDqemZ2MQSBIVE41PVXBW1tVQNLZSNTRBl1Yn15lil6gwzbdpBvLr9e-oEgsMjokbixpMnQadmdlkX9ZdWy8ylK5TqGoyZQfFjNFjKRQeTwgYQ8CBwMSnXxEUZ8cbJKaIAC0ylKuOUc20dm2ll0dj2+mOKO6-gE6MSExSFlUlSUahaVksTh+FisW2K+gcZJc7lhtPOfTIjMxzwkiEUJTWCB5xD+LhkBgqHykNM6qJIhAATjwANZgeWPZnYhDbRTEKRs5RQxoGfSKXQE1XpYiAmq6EFSfS6ZwwtxAA */
+        id: 'lightBulb',
+        initial: 'unlit',
+        states,
+        strict: true,
+    },
+    {
+        actions,
     }
-}
+);
 
-const broken = { type: 'final' }
+const service = interpret(lightBulbMachine).start();
 
-const states = { lit, unlit, broken }
-
-const config = {
-    id: 'lightBulb',
-    initial: 'unlit',
-    states,
-    strict: true
-}
-
-const lightBulbMachine = Machine({
-    id: 'lightBulb',
-    initial: 'unlit',
-    states,
-    strict: true
-})
-
-const service = interpret(
-    lightBulbMachine
-).start()
-
-service.onTransition(state => {
-
+service.onTransition((state) => {
     if (state.changed) {
-        console.log('State changed: ', state.value)
+        console.log('State changed: ', state.value);
     }
 
     if (state.matches('broken')) {
-        console.log('State is broken: ', state.value)
+        console.log('State is broken: ', state.value);
     }
-})
+});
 
-service.send('TOGGLE')
-service.send('TOGGLE')
-service.send('BREAK')
-
+service.send('TOGGLE'); // unlit -> lit
+service.send('TOGGLE'); // lit -> unlit
+service.send('BREAK');  // unlit -> broken
